665c290c5d2f3965173f8e99db4b98224aaf4de7
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-05-28 16:44:44 +0400

    feat: .env, routing created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
