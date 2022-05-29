26adaf4d4ce419349dce6744dd142f32b2455ec0
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-05-29 15:28:15 +0400

    feat: Login functionality done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../firebase";
-import { setDoc, doc, Timestamp } from "firebase/firestore";
+import { updateDoc, doc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
-const Register = () => {
+const Login = () => {
   const [data, setData] = useState({
-    name: "",
     email: "",
     password: "",
     error: null,
@@ -15,7 +14,7 @@ const Register = () => {
 
   const navigate = useNavigate();
 
-  const { name, email, password, error, loading } = data;
+  const { email, password, error, loading } = data;
 
   const handleChange = (evn) => {
     setData({ ...data, [evn.target.name]: evn.target.value });
@@ -24,24 +23,21 @@ const Register = () => {
   const handleSubmit = async (evn) => {
     evn.preventDefault();
     setData({ ...data, error: null, loading: true });
-    if (!name || !email || !password) {
+    if (!email || !password) {
       setData({ ...data, error: "All fields are required!!!" });
     }
     try {
-      // Registering new user
-      const result = await createUserWithEmailAndPassword(auth, email, password);
-      // Setting information into fireStore
-      await setDoc(doc(db, "users", result.user.uid), {
+      // Sign In a user
+      const result = await signInWithEmailAndPassword(auth, email, password);
+
+      // Updating information in fireStore
+      await updateDoc(doc(db, "users", result.user.uid), {
         // second argument: ("users") - is collection name
         // third argument: (result.user.uid) - is document
-        uid: result.user.uid,
-        name,
-        email,
-        createdAt: Timestamp.fromDate(new Date()),
         isOnline: true,
       });
       // Resetting the stat
-      setData({ name: "", email: "", password: "", error: null, loading: false });
+      setData({ email: "", password: "", error: null, loading: false });
       navigate("/");
     } catch (err) {
       setData({ ...data, error: err.message, loading: false });
@@ -50,12 +46,8 @@ const Register = () => {
 
   return (
     <section>
-      <h3>Create an account</h3>
+      <h3>Login into your account</h3>
       <form className="form" onSubmit={handleSubmit}>
-        <div className="input_container">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={handleChange} />
-        </div>
         <div className="input_container">
           <label htmlFor="email">Email</label>
           <input type="text" name="email" value={email} onChange={handleChange} />
@@ -67,11 +59,11 @@ const Register = () => {
         {error ? <p className="error">{error}</p> : null}
         <div className="btn_container">
           <button className="register_btn" disabled={loading}>
-            {loading ? "Creating ..." : "Register"}
+            {loading ? "Logging in ..." : "Login"}
           </button>
         </div>
       </form>
     </section>
   );
 };
-export default Register;
+export default Login;
