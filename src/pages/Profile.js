39f265702fa7ec70314a3e2f4632133fe66342fe
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-05-29 21:59:33 +0400

    fear: Photo upload linked with firebase storage

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Camera from "../components/svg/Camera";
-import Img from "../media/minion01.png";
+import DefaultImg from "../media/DefaultAvatar.png";
+import { storage, db, auth } from "../firebase";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
   const [img, setImg] = useState("");
-  console.log(img);
-  return (
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
+      if (docSnap.exists) {
+        setUser(docSnap.data());
+      }
+    });
+
+    if (img) {
+      const uploadImg = async () => {
+        const imgRef = ref(storage, `avatar/${new Date().getTime()} - ${img.name}`);
+        try {
+          const snap = await uploadBytes(imgRef, img);
+          const url = await getDownloadURL(ref(storage, snap.ref.fullPath));
+          await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            avatar: url,
+            avatarPath: snap.ref.fullPath,
+          });
+          setImg("");
+        } catch (err) {
+          console.log(err.message);
+        }
+      };
+      uploadImg();
+    }
+  }, [img]);
+  return user ? (
     <section>
       <div className="profile_container">
         <div className="img_container">
-          <img src={Img} alt="Avatar" />
+          <img src={user.avatar || DefaultImg} alt="Avatar" />
           <div className="overlay">
             <div>
               <label htmlFor="photo">
@@ -26,14 +55,14 @@ const Profile = () => {
           </div>
         </div>
         <div className="text_container">
-          <h3>User Name</h3>
-          <p>User Email</p>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
           <hr />
-          <small>Joined on ...</small>
+          <small>Joined on: {user.createdAt.toDate().toDateString()}</small>
         </div>
       </div>
     </section>
-  );
+  ) : null;
 };
 
 export default Profile;
