5bad02043bd461c01812f9e53c900ae2855ad886
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-05-30 14:25:48 +0400

    feat: Home page edited/userList displayed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { db, auth } from "../firebase";
+import { collection, query, where, onSnapshot, QuerySnapshot } from "firebase/firestore";
+import User from "../components/User";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const usersRef = collection(db, "users");
+    // create query object
+    const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid]));
+    // execute query
+    const unSub = onSnapshot(q, (querySnapshot) => {
+      let users = [];
+      querySnapshot.forEach((doc) => {
+        users.push(doc.data());
+      });
+      setUsers(users);
+    });
+    return () => unSub();
+  }, []);
+  console.log(users);
+  return (
+    <div className="home_container">
+      <div className="users_container">
+        {users.map((user) => (
+          <User key={user.uid} user={user} />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Home;
