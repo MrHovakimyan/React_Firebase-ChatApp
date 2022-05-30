8e8e10e095275e97a7fce9d2aba4a87738717906
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-05-30 14:52:14 +0400

    feat: userList, select user to start conversation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import User from "../components/User";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
+  const [chat, setChat] = useState("");
+
   useEffect(() => {
     const usersRef = collection(db, "users");
     // create query object
@@ -19,13 +21,27 @@ const Home = () => {
     });
     return () => unSub();
   }, []);
-  console.log(users);
+
+  const selectUser = (user) => {
+    setChat(user);
+    console.log(user);
+  };
+
   return (
     <div className="home_container">
       <div className="users_container">
         {users.map((user) => (
-          <User key={user.uid} user={user} />
+          <User key={user.uid} user={user} selectUser={selectUser} />
         ))}
+      </div>
+      <div className="messages_container">
+        {chat ? (
+          <div className="messages_user">
+            <h3>{chat.name}</h3>
+          </div>
+        ) : (
+          <h3 className="no_conv">Select a user to start conversation</h3>
+        )}
       </div>
     </div>
   );
